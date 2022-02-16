0c23c74b00d75e6f133470724948e22af5f58b03
fibercall <ravi.r@terasoftware.in>
2022-02-16 23:35:58 +0530

    testing

---
+++ Teraformtraining/vpc.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "myvpc" {
+    cidr_block = "10.40.0.0/16"
+    enable_dns_hostnames = true
+    tags = {
+        Name = "testvpc"
+	    Owner = "Sreeharsha Veerapalli"
+	    environment = "prod"
+    }
+}
